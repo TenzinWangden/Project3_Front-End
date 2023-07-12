22816bb26fc714ac9881a3f8a9b248d03690f258
KBlakeJones <119406401+KBlakeJones@users.noreply.github.com>
2023-07-12 14:09:30 -0400

    initial git status git commit -m initial

--- Warehouse/main-page/src/app/models/type.spec.ts
+++ Warehouse/main-page/src/app/models/type.spec.ts
@@ -2,6 +2,6 @@ import { Type } from './type';
 
 describe('Type', () => {
   it('should create an instance', () => {
-    expect(new Type()).toBeTruthy();
+    expect(new Type(0, "")).toBeTruthy();
   });
 });
